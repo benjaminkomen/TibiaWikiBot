ae3f4fa9f7046a1bee5a1e1270133965c0688f2f
Benjamin Komen <benjamin_komen@hotmail.com>
2017-04-21 23:26:17 +0200

    starting with next usecase.

---
+++ src/main/java/com/wikia/tibia/enums/DamageElement.java
@@ -0,0 +1,7 @@
+package com.wikia.tibia.enums;
+
+/**
+ * Created by benjamin on 21-5-2017.
+ */
+public enum DamageElement {
+}
---
+++ src/main/java/com/wikia/tibia/objects/Percentage.java
@@ -0,0 +1,7 @@
+package com.wikia.tibia.objects;
+
+/**
+ * Created by benjamin on 21-5-2017.
+ */
+public class Percentage {
+}

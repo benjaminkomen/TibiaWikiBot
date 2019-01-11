a3ced5ed0cdc10f25f6efb55299080f487f8d3f7
Benjamin Komen <komenb@gmail.com>
2019-01-11 19:27:01 +0100

    Adding some logging.

--- src/main/java/com/wikia/tibia/Main.java
+++ src/main/java/com/wikia/tibia/Main.java
@@ -2,8 +2,12 @@ package com.wikia.tibia;
 
 import com.wikia.tibia.usecases.FixCreatures;
 import com.wikia.tibia.usecases.FixItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     private Main() {
         // this class should not be constructed
@@ -11,12 +15,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // first usecase
         FixCreatures fixCreatures = new FixCreatures();
+        LOG.debug("Starting usecase FixCreatures");
         fixCreatures.checkCreatures();
 
-        // second usecase
         FixItems fixItems = new FixItems();
+        LOG.debug("Starting usecase FixItems");
         fixItems.checkItems();
     }
 }

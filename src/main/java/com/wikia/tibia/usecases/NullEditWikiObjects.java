8d37a1b92688fcb0af6637898a3492e3b679212b
Benjamin Komen <komenb@gmail.com>
2019-01-12 18:30:14 +0100

    Adding more content to NullEditWikiObjects usecase.

--- src/main/java/com/wikia/tibia/usecases/NullEditWikiObjects.java
+++ src/main/java/com/wikia/tibia/usecases/NullEditWikiObjects.java
@@ -1,12 +1,42 @@
 package com.wikia.tibia.usecases;
 
 import com.wikia.tibia.objects.Achievement;
+import com.wikia.tibia.objects.Book;
+import com.wikia.tibia.objects.Building;
+import com.wikia.tibia.objects.Corpse;
 import com.wikia.tibia.objects.Creature;
+import com.wikia.tibia.objects.Effect;
+import com.wikia.tibia.objects.HuntingPlace;
 import com.wikia.tibia.objects.Item;
+import com.wikia.tibia.objects.Key;
+import com.wikia.tibia.objects.Location;
+import com.wikia.tibia.objects.Missile;
+import com.wikia.tibia.objects.Mount;
+import com.wikia.tibia.objects.NPC;
+import com.wikia.tibia.objects.Outfit;
+import com.wikia.tibia.objects.Quest;
+import com.wikia.tibia.objects.Spell;
+import com.wikia.tibia.objects.Street;
+import com.wikia.tibia.objects.TibiaObject;
 import com.wikia.tibia.objects.WikiObject;
 import com.wikia.tibia.repositories.AchievementRepository;
+import com.wikia.tibia.repositories.BookRepository;
+import com.wikia.tibia.repositories.BuildingRepository;
+import com.wikia.tibia.repositories.CorpseRepository;
 import com.wikia.tibia.repositories.CreatureRepository;
+import com.wikia.tibia.repositories.EffectRepository;
+import com.wikia.tibia.repositories.HuntingPlaceRepository;
 import com.wikia.tibia.repositories.ItemRepository;
+import com.wikia.tibia.repositories.KeyRepository;
+import com.wikia.tibia.repositories.LocationRepository;
+import com.wikia.tibia.repositories.MissileRepository;
+import com.wikia.tibia.repositories.MountRepository;
+import com.wikia.tibia.repositories.NPCRepository;
+import com.wikia.tibia.repositories.ObjectRepository;
+import com.wikia.tibia.repositories.OutfitRepository;
+import com.wikia.tibia.repositories.QuestRepository;
+import com.wikia.tibia.repositories.SpellRepository;
+import com.wikia.tibia.repositories.StreetRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,20 +50,65 @@ import java.util.List;
 public class NullEditWikiObjects {
 
     private static final Logger LOG = LoggerFactory.getLogger(NullEditWikiObjects.class);
-    private static final boolean DEBUG_MODE = false;
+    private static final boolean DEBUG_MODE = true;
     private static final String LOG_SAVE_ARTICLE = "Saving article {}";
 
     private AchievementRepository achievementRepository;
+    private BookRepository bookRepository;
+    private BuildingRepository buildingRepository;
+    private CorpseRepository corpseRepository;
     private CreatureRepository creatureRepository;
+    private EffectRepository effectRepository;
+    private HuntingPlaceRepository huntingPlaceRepository;
     private ItemRepository itemRepository;
+    private KeyRepository keyRepository;
+    private LocationRepository locationRepository;
+    private MissileRepository missileRepository;
+    private MountRepository mountRepository;
+    private NPCRepository npcRepository;
+    private ObjectRepository objectRepository;
+    private OutfitRepository outfitRepository;
+    private QuestRepository questRepository;
+    private SpellRepository spellRepository;
+    private StreetRepository streetRepository;
     private List<Achievement> achievements = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
+    private List<Building> buildings = new ArrayList<>();
+    private List<Corpse> corpses = new ArrayList<>();
     private List<Creature> creatures = new ArrayList<>();
+    private List<Effect> effects = new ArrayList<>();
+    private List<HuntingPlace> huntingPlaces = new ArrayList<>();
     private List<Item> items = new ArrayList<>();
+    private List<Key> keys = new ArrayList<>();
+    private List<Location> locations = new ArrayList<>();
+    private List<Missile> missiles = new ArrayList<>();
+    private List<Mount> mounts = new ArrayList<>();
+    private List<NPC> npcs = new ArrayList<>();
+    private List<TibiaObject> objects = new ArrayList<>();
+    private List<Outfit> outfits = new ArrayList<>();
+    private List<Quest> quests = new ArrayList<>();
+    private List<Spell> spells = new ArrayList<>();
+    private List<Street> streets = new ArrayList<>();
 
     public NullEditWikiObjects() {
         this.achievementRepository = new AchievementRepository();
+        this.bookRepository = new BookRepository();
+        this.buildingRepository = new BuildingRepository();
+        this.corpseRepository = new CorpseRepository();
         this.creatureRepository = new CreatureRepository();
+        this.effectRepository = new EffectRepository();
+        this.huntingPlaceRepository = new HuntingPlaceRepository();
         this.itemRepository = new ItemRepository();
+        this.keyRepository = new KeyRepository();
+        this.locationRepository = new LocationRepository();
+        this.missileRepository = new MissileRepository();
+        this.mountRepository = new MountRepository();
+        this.npcRepository = new NPCRepository();
+        this.objectRepository = new ObjectRepository();
+        this.outfitRepository = new OutfitRepository();
+        this.questRepository = new QuestRepository();
+        this.spellRepository = new SpellRepository();
+        this.streetRepository = new StreetRepository();
     }
 
     public void checkAchievements() {
@@ -46,11 +121,62 @@ public class NullEditWikiObjects {
                 );
     }
 
+    public void checkBooks() {
+        getBooks()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkBuildings() {
+        getBuildings()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkCorpses() {
+        getCorpses()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+
     public void checkCreatures() {
         getCreatures()
                 .forEach(creature -> {
                             LOG.info(LOG_SAVE_ARTICLE, creature.getName());
                             saveArticle(creature);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkEffects() {
+        getEffects()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkHuntingPlaces() {
+        getHuntingPlaces()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
                             pauseForABit();
                         }
                 );
@@ -66,11 +192,132 @@ public class NullEditWikiObjects {
                 );
     }
 
+    public void checkKeys() {
+        getKeys()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkLocations() {
+        getLocations()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkMissiles() {
+        getMissiles()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkMounts() {
+        getMounts()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkNPCs() {
+        getNPCs()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkObjects() {
+        getObjects()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkOutfits() {
+        getOutfits()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkQuests() {
+        getQuests()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkSpells() {
+        getSpells()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
+    public void checkStreets() {
+        getStreets()
+                .forEach(wikiObject -> {
+                            LOG.info(LOG_SAVE_ARTICLE, wikiObject.getName());
+                            saveArticle(wikiObject);
+                            pauseForABit();
+                        }
+                );
+    }
+
     private List<Achievement> getAchievements() {
         if (achievements == null || achievements.isEmpty()) {
             achievements = achievementRepository.getWikiObjects();
         }
         return achievements;
+    }
+
+    private List<Book> getBooks() {
+        if (books == null || books.isEmpty()) {
+            books = bookRepository.getWikiObjects();
+        }
+        return books;
+    }
+
+    private List<Building> getBuildings() {
+        if (buildings == null || buildings.isEmpty()) {
+            buildings = buildingRepository.getWikiObjects();
+        }
+        return buildings;
+    }
+
+    private List<Corpse> getCorpses() {
+        if (corpses == null || corpses.isEmpty()) {
+            corpses = corpseRepository.getWikiObjects();
+        }
+        return corpses;
     }
 
     private List<Creature> getCreatures() {
@@ -80,11 +327,95 @@ public class NullEditWikiObjects {
         return creatures;
     }
 
+    private List<Effect> getEffects() {
+        if (effects == null || effects.isEmpty()) {
+            effects = effectRepository.getWikiObjects();
+        }
+        return effects;
+    }
+
+    private List<HuntingPlace> getHuntingPlaces() {
+        if (huntingPlaces == null || huntingPlaces.isEmpty()) {
+            huntingPlaces = huntingPlaceRepository.getWikiObjects();
+        }
+        return huntingPlaces;
+    }
+
     private List<Item> getItems() {
         if (items == null || items.isEmpty()) {
             items = itemRepository.getWikiObjects();
         }
         return items;
+    }
+
+    private List<Key> getKeys() {
+        if (keys == null || keys.isEmpty()) {
+            keys = keyRepository.getWikiObjects();
+        }
+        return keys;
+    }
+
+    private List<Location> getLocations() {
+        if (locations == null || locations.isEmpty()) {
+            locations = locationRepository.getWikiObjects();
+        }
+        return locations;
+    }
+
+    private List<Missile> getMissiles() {
+        if (missiles == null || missiles.isEmpty()) {
+            missiles = missileRepository.getWikiObjects();
+        }
+        return missiles;
+    }
+
+    private List<Mount> getMounts() {
+        if (mounts == null || mounts.isEmpty()) {
+            mounts = mountRepository.getWikiObjects();
+        }
+        return mounts;
+    }
+
+    private List<NPC> getNPCs() {
+        if (npcs == null || npcs.isEmpty()) {
+            npcs = npcRepository.getWikiObjects();
+        }
+        return npcs;
+    }
+
+    private List<TibiaObject> getObjects() {
+        if (objects == null || objects.isEmpty()) {
+            objects = objectRepository.getWikiObjects();
+        }
+        return objects;
+    }
+
+    private List<Outfit> getOutfits() {
+        if (outfits == null || outfits.isEmpty()) {
+            outfits = outfitRepository.getWikiObjects();
+        }
+        return outfits;
+    }
+
+    private List<Quest> getQuests() {
+        if (quests == null || quests.isEmpty()) {
+            quests = questRepository.getWikiObjects();
+        }
+        return quests;
+    }
+
+    private List<Spell> getSpells() {
+        if (spells == null || spells.isEmpty()) {
+            spells = spellRepository.getWikiObjects();
+        }
+        return spells;
+    }
+
+    private List<Street> getStreets() {
+        if (streets == null || streets.isEmpty()) {
+            streets = streetRepository.getWikiObjects();
+        }
+        return streets;
     }
 
     private void saveArticle(WikiObject wikiObject) {
--- src/test/java/com/wikia/tibia/usecases/NullEditWikiObjectsTest.java
+++ src/test/java/com/wikia/tibia/usecases/NullEditWikiObjectsTest.java
@@ -20,8 +20,33 @@ public class NullEditWikiObjectsTest {
     }
 
     @Test
+    public void testCheckBooks() {
+        target.checkBooks();
+    }
+
+    @Test
+    public void testCheckBuildings() {
+        target.checkBuildings();
+    }
+
+    @Test
+    public void testCheckCorpses() {
+        target.checkCorpses();
+    }
+
+    @Test
     public void testCheckCreatures() {
         target.checkCreatures();
+    }
+
+    @Test
+    public void testCheckEffects() {
+        target.checkEffects();
+    }
+
+    @Test
+    public void testCheckHuntingPlaces() {
+        target.checkHuntingPlaces();
     }
 
     @Test
@@ -29,4 +54,53 @@ public class NullEditWikiObjectsTest {
         target.checkItems();
     }
 
+    @Test
+    public void testCheckKeys() {
+        target.checkKeys();
+    }
+
+    @Test
+    public void testCheckLocations() {
+        target.checkLocations();
+    }
+
+    @Test
+    public void testCheckMissiles() {
+        target.checkMissiles();
+    }
+
+    @Test
+    public void testCheckMounts() {
+        target.checkMounts();
+    }
+
+    @Test
+    public void testCheckNPCs() {
+        target.checkNPCs();
+    }
+
+    @Test
+    public void testCheckObjects() {
+        target.checkObjects();
+    }
+
+    @Test
+    public void testCheckOutfits() {
+        target.checkOutfits();
+    }
+
+    @Test
+    public void testCheckQuests() {
+        target.checkQuests();
+    }
+
+    @Test
+    public void testCheckSpells() {
+        target.checkSpells();
+    }
+
+    @Test
+    public void testCheckStreets() {
+        target.checkStreets();
+    }
 }

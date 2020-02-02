e03e018ccc71cff56660372d26242920cbd7b791
Benjamin Komen <komenb@gmail.com>
2020-02-02 16:09:52 +0100

    Fix bug and fix unit tests.

--- src/main/java/com/wikia/tibia/objects/LootStatisticsItem.java
+++ src/main/java/com/wikia/tibia/objects/LootStatisticsItem.java
@@ -1,19 +1,23 @@
 package com.wikia.tibia.objects;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
+@Slf4j
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class LootStatisticsItem {
 
-    private final String NUMBER_REGEX = "(\\d+)";
-    private final String NUMBER_RANGE_REGEX = "(\\d+)-(\\d+)";
+    private static final String NUMBER_REGEX = "^(\\d+)$";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile(NUMBER_REGEX);
+    private static final String NUMBER_RANGE_REGEX = "^(\\d+)-(\\d+)$";
+    private static final Pattern NUMBER_RANGE_PATTERN = Pattern.compile(NUMBER_RANGE_REGEX);
     private String itemName;
     private String times;
     private String amount;
@@ -45,16 +49,32 @@ public class LootStatisticsItem {
     }
 
     private String sumStringsAsInt(String left, String right) {
+        if (left == null && right == null) {
+            return "";
+        } else if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        }
         return String.valueOf(Integer.parseInt(left) + Integer.parseInt(right));
     }
 
     private String sumAmounts(String left, String right) {
+
+        if (left == null && right == null) {
+            return "";
+        } else if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        }
+
         // if both are a single number, take the maximum
         if (left.matches(NUMBER_REGEX) && right.matches(NUMBER_REGEX)) {
-            String[] leftMatches = getMatches(left);
+            String[] leftMatches = getMatches(NUMBER_PATTERN, left);
             var leftUpper = leftMatches[0];
 
-            String[] rightMatches = getMatches(right);
+            String[] rightMatches = getMatches(NUMBER_PATTERN, right);
             var rightUpper = rightMatches[0];
 
             var newUpper = Math.max(Integer.parseInt(leftUpper), Integer.parseInt(rightUpper));
@@ -64,10 +84,12 @@ public class LootStatisticsItem {
 
         // if both are a range, take the lowest to the highest as new range
         if (left.matches(NUMBER_RANGE_REGEX) && right.matches(NUMBER_RANGE_REGEX)) {
-            String[] leftMatches = getMatches(left);
+            String[] leftMatches = getMatches(NUMBER_RANGE_PATTERN, left);
+            leftMatches = leftMatches[0].split("-");
             var leftLower = leftMatches[0];
             var leftUpper = leftMatches[1];
-            String[] rightMatches = getMatches(right);
+            String[] rightMatches = getMatches(NUMBER_RANGE_PATTERN, right);
+            rightMatches = rightMatches[0].split("-");
             var rightLower = rightMatches[0];
             var rightUpper = rightMatches[1];
 
@@ -77,11 +99,12 @@ public class LootStatisticsItem {
             return String.format("%s-%s", newLower, newUpper);
         }
 
-        return left; // TODO this is not the best default
+        log.warn("Could not correctly sum amounts, neither left ({}) nor right ({}) is a valid number.", left, right);
+        return "";
     }
 
-    private String[] getMatches(String left) {
-        return Pattern.compile(NUMBER_RANGE_REGEX)
+    private String[] getMatches(Pattern pattern, String left) {
+        return pattern
                 .matcher(left)
                 .results()
                 .map(MatchResult::group)

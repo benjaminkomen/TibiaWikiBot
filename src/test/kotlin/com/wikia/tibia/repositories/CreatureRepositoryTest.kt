54f01d62c1730b9070b85ee4d12cf6b9d6adfecd
Benjamin Komen <benjamin_komen@hotmail.com>
2020-06-01 10:46:34 +0200

    Added one more test.

--- src/test/kotlin/com/wikia/tibia/repositories/CreatureRepositoryTest.kt
+++ src/test/kotlin/com/wikia/tibia/repositories/CreatureRepositoryTest.kt
@@ -2,13 +2,15 @@ package com.wikia.tibia.repositories
 
 import com.wikia.tibia.gateways.WikiObjectGateway
 import com.wikia.tibia.objects.Creature
+import io.vavr.control.Try
 import org.hamcrest.MatcherAssert
 import org.hamcrest.Matchers
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.mockito.Mock
-import kotlin.reflect.full.findParameterByName
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.lang.reflect.Field
 
 class CreatureRepositoryTest {
 
@@ -16,22 +18,44 @@ class CreatureRepositoryTest {
     private lateinit var creatureGateway: WikiObjectGateway<Creature>
     private lateinit var creatureRepository: CreatureRepository
 
+    @Suppress("UNCHECKED_CAST")
     @Before
     fun setup() {
-//        creatureGateway = Mockito.mock(WikiObjectGateway::class.java)
+        creatureGateway = mock(WikiObjectGateway::class.java) as WikiObjectGateway<Creature>
         creatureRepository = CreatureRepository()
-        val field = ::creatureRepository.findParameterByName("wikiObjectGateway")
+        `inject mocked gateway`(creatureRepository, creatureGateway)
     }
 
-    @Ignore // TODO finish writing this test (inject wikiObjectGateway with reflection?)
     @Test
-    fun `should get empty list of LootWrapper objects`() {
+    fun `should get empty list of Creature objects`() {
         val someJson = """[]"""
-//        `when`(lootGateway.getLoot(true)).thenReturn(Try.success(someJson))
+        `when`(creatureGateway.getWikiObjects(true)).thenReturn(Try.success(someJson))
 
         val result = creatureRepository.getWikiObjects()
 
         MatcherAssert.assertThat(result.isSuccess, Matchers.`is`(true))
         MatcherAssert.assertThat(result.get().size, Matchers.`is`(0))
+    }
+
+    @Test
+    fun `should get list of two Creature objects`() {
+        val someJson = getResourceAsText("/creatures.json")
+        `when`(creatureGateway.getWikiObjects(true)).thenReturn(Try.success(someJson))
+
+        val result = creatureRepository.getWikiObjects()
+
+        MatcherAssert.assertThat(result.isSuccess, Matchers.`is`(true))
+        MatcherAssert.assertThat(result.get().size, Matchers.`is`(2))
+    }
+
+    // TODO it would be cool to rewrite this to Kotlin some time
+    private fun `inject mocked gateway`(creatureRepository: CreatureRepository, creatureGateway: WikiObjectGateway<Creature>) {
+        val field: Field = creatureRepository.javaClass.superclass.getDeclaredField("wikiObjectGateway")
+        field.trySetAccessible()
+        field.set(creatureRepository, creatureGateway)
+    }
+
+    private fun getResourceAsText(path: String): String? {
+        return object {}.javaClass.getResource(path)?.readText()
     }
 }
